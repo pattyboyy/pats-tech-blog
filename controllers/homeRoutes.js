cc7571eeff83555f8e564315fcbd9501992972a1
pattyboyy <134738449+pattyboyy@users.noreply.github.com>
2024-06-10 12:52:42 -0600

    test

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,26 @@
+const router = require('express').Router();
+const { Post, User } = require('../models');
+
+router.get('/', async (req, res) => {
+  try {
+    const postData = await Post.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['username'],
+        },
+      ],
+    });
+
+    const posts = postData.map((post) => post.get({ plain: true }));
+
+    res.render('homepage', {
+      posts,
+      loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
---
+++ public/js/comment.js
@@ -0,0 +1,24 @@
+const commentFormHandler = async (event) => {
+    event.preventDefault();
+
+    const comment_text = document.querySelector('#comment-text').value.trim();
+    const post_id = document.querySelector('#post-id').value;
+
+    if (comment_text) {
+        const response = await fetch('/api/comments', {
+            method: 'POST',
+            body: JSON.stringify({ post_id, comment_text }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            document.location.reload();
+        } else {
+            alert('Failed to add comment.');
+        }
+    }
+};
+
+document
+    .querySelector('.comment-form')
+    .addEventListener('submit', commentFormHandler);
